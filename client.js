c4506eacd30d3b979658f2734ec29b5a666a8ce0
Jason Peppers <jasonpeppers72@gmail.com>
2020-12-05 22:37:02 -0500

    remove old code

--- client.js
+++ client.js
@@ -29,27 +29,3 @@ fetch('http://localhost:3000/json', {
     .catch((err) => {
         console.log(`Error: ${err}`);
     });
-
-// http.request(
-//     {
-//         hostname: 'localhost',
-//         port: 3000,
-//         path: '/',
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/x-www-form-urlencoded',
-//             'Content-Length': postData.length,
-//         },
-//     },
-//     (res) => {
-//         // console.log(`statusCode = ${res.statusCode}`);
-//         res.on('data', (d) => {
-//             console.log(`Response: ${d}`);
-//         });
-//     }
-// )
-//     .on('error', (err) => {
-//         console.log('Error with http request');
-//         console.log(`${err}`);
-//     })
-//     .write(postData);
