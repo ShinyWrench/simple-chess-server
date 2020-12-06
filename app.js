af48e0a20be688fc6d22bef01c57371cf11de3c8
Jason Peppers <jasonpeppers72@gmail.com>
2020-12-05 21:43:16 -0500

    server: handle json requests and urlencoded requests in different routes; client: use node-fetch instead of http module for requests; client: send separate x-www-form-urlencoded and json requests

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-// const jsonParser = bodyParser.json();
+const jsonParser = bodyParser.json();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get('/', (req, res) => {
     res.send('Hiya buddy!');
 });
 
-app.post('/', urlencodedParser, (req, res) => {
-    res.send(`This is what you said: ${JSON.stringify(req.body)}`);
+app.post('/json/', jsonParser, (req, res) => {
+    res.send(`Decoding JSON: ${JSON.stringify(req.body)}`);
+});
+
+app.post('/urlencoded/', urlencodedParser, (req, res) => {
+    res.send(`Decoding urlencoded: ${JSON.stringify(req.body)}`);
 });
 
 app.listen(port);
--- client.js
+++ client.js
@@ -1,28 +1,55 @@
-const http = require('http');
-const querystring = require('querystring');
+const fetch = require('node-fetch');
+const { URLSearchParams } = require('url');
 
-let postData = querystring.stringify({ message: 'Hiya buddy!' });
+let postParams = new URLSearchParams();
+postParams.append('message', 'Whats up!');
 
-http.request(
-    {
-        hostname: 'localhost',
-        port: 3000,
-        path: '/',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': postData.length,
-        },
-    },
-    (res) => {
-        // console.log(`statusCode = ${res.statusCode}`);
-        res.on('data', (d) => {
-            console.log(`Response: ${d}`);
-        });
-    }
-)
-    .on('error', (err) => {
-        console.log('Error with http request');
-        console.log(`${err}`);
+fetch('http://localhost:3000/urlencoded', {
+    method: 'POST',
+    body: postParams,
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+})
+    .then((res) => res.text())
+    .then((text) => {
+        console.log(`text: ${text}`);
     })
-    .write(postData);
+    .catch((err) => {
+        console.log(`Error: ${err}`);
+    });
+
+fetch('http://localhost:3000/json', {
+    method: 'POST',
+    body: JSON.stringify({ message: 'Howdy!' }),
+    headers: { 'Content-Type': 'application/json' },
+})
+    .then((res) => res.text())
+    .then((text) => {
+        console.log(`text: ${text}`);
+    })
+    .catch((err) => {
+        console.log(`Error: ${err}`);
+    });
+
+// http.request(
+//     {
+//         hostname: 'localhost',
+//         port: 3000,
+//         path: '/',
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/x-www-form-urlencoded',
+//             'Content-Length': postData.length,
+//         },
+//     },
+//     (res) => {
+//         // console.log(`statusCode = ${res.statusCode}`);
+//         res.on('data', (d) => {
+//             console.log(`Response: ${d}`);
+//         });
+//     }
+// )
+//     .on('error', (err) => {
+//         console.log('Error with http request');
+//         console.log(`${err}`);
+//     })
+//     .write(postData);
