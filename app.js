d2df2be3d400e24b85f681a559693721e1eea992
Jason Peppers <jasonpeppers72@gmail.com>
2020-12-11 16:57:54 -0500

    remove unused routes

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const chessController = require('./chessController');
 
 chessController.init();
 
 const app = express();
 const port = 3000;
-const jsonParser = bodyParser.json();
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-app.get('/', (req, res) => {
-    res.send('Hiya buddy!');
-});
-
-app.post('/json/', jsonParser, (req, res) => {
-    let response = `Decoded JSON: ${JSON.stringify(req.body)}`;
-    console.log(response);
-    res.send(response);
-});
-
-app.post('/urlencoded/', urlencodedParser, (req, res) => {
-    let response = `Decoded urlencoded: ${JSON.stringify(req.body)}`;
-    console.log(response);
-    res.send(response);
-});
 
 // TODO: replace with regex pattern to match app.get('/e4'), etc.
 app.get('/chess/:move', chessController.play);
