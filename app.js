5971229d3d71882f1a2f9432a38c4fbc00b6cc35
Jason Peppers <jasonpeppers72@gmail.com>
2020-12-12 12:45:08 -0500

    DRY: use loadGame middleware

--- app.js
+++ app.js
@@ -6,12 +6,11 @@ chessController.init();
 const app = express();
 const port = 3000;
 
+app.use(chessController.loadGame);
+
 app.get('/go', chessController.makeEngineMove);
-
 app.get('/moves', chessController.getMoves);
-
 app.get('/resign', chessController.resign);
-
 // TODO: replace with regex pattern to match app.get('/e4'), etc.
 app.get('/:move', chessController.move);
 
