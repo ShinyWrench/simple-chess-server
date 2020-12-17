74273d5962570eb46ad92458508f900806b8decf
Jason Peppers <jasonpeppers72@gmail.com>
2020-12-16 23:38:19 -0500

    allow client to request JSON response (instead of text)

--- chessController.js
+++ chessController.js
@@ -1,16 +1,30 @@
 const ChessGame = require('./ChessGame');
 
 // TODO:
-//       set up default 'keepAlive' arg in ChessGame move & resign methods
-//           controls whether 'positionReporter' is updated, engine moves are updated
-//       '/board' or '/showboard' route
-//           show board or toggle whether to include in every move response
 //       allow client to change difficulty level
 //       allow client to get a list of commands ('/help')
+//       '/board' or '/showboard' route
+//           show board or toggle whether to include in every move response
 //       replace CSV/JSON with actual DB (Mongo?)
 
 function init() {
     ChessGame.initEngine();
+}
+
+function respond(req, res, message) {
+    if ('jsonresponse' in req.query && req.query.jsonresponse === 'true') {
+        res.json(message);
+    } else {
+        let output = '';
+        output = `${output}${message.error ? message.error + ',' : ''}`;
+        output = `${output}${message.move ? message.move + ',' : ''}`;
+        output = `${output}${message.moves ? message.moves + ',' : ''}`;
+        output = `${output}${message.status ? message.status + ',' : ''}`;
+        if (output.length > 0) {
+            output = output.slice(0, -1);
+        }
+        res.send(output);
+    }
 }
 
 async function loadGame(req, res, next) {
@@ -21,7 +35,7 @@ async function loadGame(req, res, next) {
         next();
     } catch (err) {
         console.log(`Error in loadGame:\n${err.stack ? err.stack : err}`);
-        res.send('error');
+        respond(req, res, { error: 'error' });
     }
 }
 
@@ -29,19 +43,21 @@ async function move(req, res) {
     try {
         let clientMove = req.params.move;
         if ((await req.chessGame.validateMove(clientMove)) === false) {
-            res.send('error');
+            respond(req, res, { error: 'invalid move' });
             return;
         } else {
             await req.chessGame.move(clientMove);
             if (req.chessGame.isGameOver()) {
-                res.send(req.chessGame.getEndOfGameState());
+                respond(req, res, {
+                    status: req.chessGame.getEndOfGameState(),
+                });
                 return;
             }
         }
         await makeEngineMove(req, res);
     } catch (err) {
         console.log(`Error in move:\n${err.stack ? err.stack : err}`);
-        res.send('error');
+        respond(req, res, { error: 'error' });
     }
 }
 
@@ -49,34 +65,37 @@ async function makeEngineMove(req, res) {
     try {
         let engineMove = await req.chessGame.makeEngineMove();
         if (req.chessGame.isGameOver()) {
-            res.send(`${engineMove},${req.chessGame.getEndOfGameState()}`);
+            respond(req, res, {
+                move: engineMove,
+                status: req.chessGame.getEndOfGameState(),
+            });
             return;
         } else {
-            res.send(engineMove);
+            respond(req, res, { move: engineMove });
             return;
         }
     } catch (err) {
         console.log(`Error in makeEngineMove:\n${err.stack ? err.stack : err}`);
-        res.send('error');
+        respond(req, res, { error: 'error' });
     }
 }
 
 async function getMoves(req, res) {
     try {
-        res.send(req.chessGame.getMoveHistory());
+        respond(req, res, { moves: req.chessGame.getMoveHistory() });
     } catch (err) {
         console.log(`Error in getMoves:\n${err.stack ? err.stack : err}`);
-        res.send('error');
+        respond(req, res, { error: 'error' });
     }
 }
 
 async function resign(req, res) {
     try {
         await req.chessGame.resign();
-        res.send('OK');
+        respond(req, res, { state: 'OK' });
     } catch (err) {
         console.log(`Error in resign:\n${err.stack ? err.stack : err}`);
-        res.send('error');
+        respond(req, res, { error: 'error' });
     }
 }
 
--- client.js
+++ client.js
@@ -3,21 +3,15 @@ ChessGame.initEngine();
 const fetch = require('node-fetch');
 const { engine } = require('./constants');
 
-// TODO: Clean and commit current work
-// TODO (next commit):
-//          Fix this: when server responds with "move,endofgamestate", the
-//          engine command after right here isn't working (and that's why
-//          the last move is not displayed in the log. Fix this by doing a
-//          JSON response from the server with move and state instead of a
-//          string.
-//          (temp fix is in place for now: replace as described above)
-// TODO: (spread across commits after last commit):
+// TODO:
+//           CODE FASTER -- LIVE DANGEROUSLY (for now -- learn
+//               to & write a tester after you get to the S3 fun)
+//           Make move request with optional query params
+//              .../e2e4&depth=7&error=...
 //           Identify players in storage record with
 //               name(in addition to ip address)-- use compound key?
 //           Control whether to do console.log calls with a ChessGame
 //               constructor object param
-//           Make move request with optional query params
-//              .../e2e4&depth=7&error=...
 //           See if there are other ways to speed up gameplay
 //           Keep track of black, white (?)
 // TODO: (after all of the above)
@@ -27,7 +21,9 @@ const serverAddress = 'http://localhost:3000';
 
 async function playOneGame() {
     // Resign if in the middle of a game
-    let existingMoves = await (await fetch(`${serverAddress}/moves`)).text();
+    let existingMoves = (
+        await (await fetch(`${serverAddress}/moves?jsonresponse=true`)).json()
+    ).moves;
     if (existingMoves.length > 0) {
         await fetch(`${serverAddress}/resign`);
     }
@@ -39,14 +35,17 @@ async function playOneGame() {
     while (true) {
         let engineMove = await chessGame.makeEngineMove(7);
         console.log(`My move: ${engineMove}`);
-        let serverMove = await (
-            await fetch(`${serverAddress}/${engineMove}`)
-        ).text();
-        console.log(`Server move: ${serverMove}`);
-        if (serverMove.includes(',')) {
-            serverMove = serverMove.split(',')[0];
+        let response = await (
+            await fetch(`${serverAddress}/${engineMove}?jsonresponse=true`)
+        ).json();
+        if ('move' in response) {
+            console.log(`Server move: ${response.move}`);
         }
-        serverMove = await chessGame.move(serverMove);
+        if ('status' in response) {
+            console.log(`Server reports status '${response.status}'`);
+            return;
+        }
+        await chessGame.move(response.move);
         if (chessGame.isGameOver()) {
             console.log(`${chessGame.getEndOfGameState()}`);
             return;
