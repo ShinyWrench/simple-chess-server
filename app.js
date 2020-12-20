93f3a0d8c07de64531ef8254316080144caafe90
Jason Peppers <jasonpeppers72@gmail.com>
2020-12-20 13:40:46 -0500

    improve and simplify overall route structure

--- app.js
+++ app.js
@@ -9,19 +9,36 @@ const port = 3000;
 
 app.use(chessController.loadGame);
 
+// Start a new game
 app.get(
-    '/go',
-    [query('depth').optional().isInt(), query('skill').optional().isInt()],
-    chessController.makeEngineMove
+    '/start',
+    [query('player').optional()],
+    [query('color').optional()],
+    [query('depth').optional()],
+    [query('skill').optional()],
+    chessController.startGame
 );
-app.get('/moves', chessController.getMoves);
-app.get('/resign', chessController.resign);
+
+// Set opponent engine depth and skill for ip address or player at ip address
+app.get(
+    '/config',
+    [
+        [query('player').optional()],
+        query('depth').optional().isInt(),
+        query('skill').optional().isInt(),
+    ],
+    chessController.config
+);
+
+// Resign the current game for ip address or player at ip address
+app.get('/resign', [query('player').optional()], chessController.resign);
+
+// Get current status for ip address or player at ip address
+app.get('/status', [query('player').optional()], chessController.getStatus);
+
+// Move a piece in the current game for ip address or player at ip address
 // TODO: replace with regex pattern to match app.get('/e4'), etc.
-app.get(
-    '/:move',
-    [query('depth').optional().isInt(), query('skill').optional().isInt()],
-    chessController.move
-);
+app.get('/:move', [query('player').optional()], chessController.move);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
--- client.js
+++ client.js
@@ -4,39 +4,66 @@ const fetch = require('node-fetch');
 const { engine } = require('./constants');
 
 // TODO:
+//           Have ChessGame keep track of player names and black and white
+//               whoever calls ChessGame.move should provide the player name
+//                   (whether from outside the class or within another method)
+//           Also track engine skill and depth
 //           Identify players in storage record with
 //               name(in addition to ip address)-- use compound key?
 //           Control whether to do console.log calls with a ChessGame
 //               constructor object param
 //           See if there are other ways to speed up gameplay
-//           Keep track of black, white (?)
+//           Record total piece value differential for each side when game ends (+n or -n)
 // TODO: (after all of the above)
 //           Run multiple servers, hit with one client
 
 const serverAddress = 'http://localhost:3000';
 
+function randomTrueOrFalse() {
+    return Math.random() < 0.5;
+}
+
 async function playOneGame() {
     // Resign if in the middle of a game
-    let existingMoves = (
-        await (await fetch(`${serverAddress}/moves?jsonresponse=true`)).json()
-    ).moves;
-    if (existingMoves.length > 0) {
+    let status = await (
+        await fetch(`${serverAddress}/status?jsonresponse=true&player=Billy`)
+    ).json();
+    if (status.inGame === 'true') {
         await fetch(`${serverAddress}/resign`);
     }
 
-    // Start a ChessGame
-    let chessGame = await ChessGame.startNewLocalGame();
+    // Set opponent engine skill and depth for player
+    await fetch(
+        `${serverAddress}/config?jsonresponse=true&skill=18&depth=6&player=Billy`
+    );
 
-    // Play a game against the server
+    // Start a local ChessGame with my color and engine details
+    let gameParams = {
+        color: randomTrueOrFalse() === true ? 'white' : 'black',
+        skill: 20,
+        depth: 10,
+    };
+    let chessGame = await ChessGame.startNewLocalGame(gameParams);
+
+    // Begin the game with the server
+    let response = await (
+        await fetch(
+            `${serverAddress}/start?jsonresponse=true&player=Billy&color=${gameParams.color}&skill=19`
+        )
+    ).json();
+
+    // If we are black, play server's first move in the local ChessGame
+    if (gameParams.color === 'black') {
+        await chessGame.move(response.move);
+    }
+
+    // Play (the rest of) the game against the server
     while (true) {
-        let engineMove = await chessGame.makeEngineMove({
-            depth: 8,
-            skill: 20,
-        });
+        let engineMove = await chessGame.makeEngineMove();
         console.log(`My move: ${engineMove}`);
         let response = await (
             await fetch(
-                `${serverAddress}/${engineMove}?jsonresponse=true&skill=18&depth=6`
+                `${serverAddress}/${engineMove}?jsonresponse=true&player=Billy`
             )
         ).json();
         if ('move' in response) {
