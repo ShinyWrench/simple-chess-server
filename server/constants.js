d1a803eb298d8690d6788503d5e24d4c0d7f8e66
Jason Peppers <jasonpeppers72@gmail.com>
2020-12-27 12:14:56 -0500

    separate client and server files, trim both package.json files

---
+++ server/constants.js
@@ -0,0 +1,12 @@
+module.exports = {
+    commands: {
+        setMoves: 'position startpos moves',
+        display: 'd',
+        requestMove: 'go depth ',
+        setSkillLevel: 'setoption name Skill Level value ',
+    },
+    engine: {
+        defaultDepth: 9,
+        defaultSkill: 20,
+    },
+};
